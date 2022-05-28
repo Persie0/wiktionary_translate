e8a4165eb15badc020cf38e285fc9896f581004b
Persie0 <hashcod3@protonmail.com>
2022-05-28 21:39:07 +0200

    init

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'wiktionary_translate',         # How you named your package folder (MyLib)
   packages = ['wiktionary_translate'],   # Chose the same as "name"
-  version = '0.0.1',      # Start with a small number and increase it with every change you make
+  version = '0.0.2',      # Start with a small number and increase it with every change you make
   license='mpl-2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Get English translations of foreign word with Wiktionary',   # Give a short description about your library
   author = 'Marvin perzi',                   # Type in your name
   author_email = 'hashcod3@protonmail.com',      # Type in your E-Mail
   url = 'https://github.com/Persie0/wiktionary_translate',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Persie0/wiktionary_translate/archive/refs/tags/v0.0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Persie0/wiktionary_translate/archive/refs/tags/v0.0.2.tar.gz',    # I explain this later on
   keywords = ['wiktionary', 'dictionary'],   # Keywords that define your package best
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
