e49f1ca125a2b3777c73da3a1c508490f910251f
Persie0 <hashcod3@protonmail.com>
2022-05-30 12:34:19 +0200

    init

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
-from distutils.core import setup
-setup(
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
   name = 'wiktionary_translate',         # How you named your package folder (MyLib)
   packages = ['wiktionary_translate'],   # Chose the same as "name"
-  version = '0.0.2',      # Start with a small number and increase it with every change you make
+  version = '0.0.5',      # Start with a small number and increase it with every change you make
   license='mpl-2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Get English translations of foreign word with Wiktionary',   # Give a short description about your library
-  author = 'Marvin perzi',                   # Type in your name
+  author = 'Marvin Perzi',                   # Type in your name
   author_email = 'hashcod3@protonmail.com',      # Type in your E-Mail
-  long_description = 'file: README.md',
+  long_description = long_description,
   long_description_content_type = 'text/markdown',
   url = 'https://github.com/Persie0/wiktionary_translate',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Persie0/wiktionary_translate/archive/refs/tags/v0.0.2.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Persie0/wiktionary_translate/archive/refs/tags/v0.0.5.tar.gz',    # I explain this later on
   keywords = ['wiktionary', 'dictionary'],   # Keywords that define your package best
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
